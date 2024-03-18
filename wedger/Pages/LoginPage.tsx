c9516b2e966396d394d3db469ef37f9a96a51e18
Max Shroyer <91408516+Shark9y@users.noreply.github.com>
2024-03-18 14:13:45 -0500

    Added base pages and boiler plate code

---
+++ wedger/Pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class LoginPage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LoginPage</Text>
+      </View>
+    )
+  }
+}
+
+export default LoginPage
---
+++ wedger/Pages/WelcomePage.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class WelcomePage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>WelcomePage</Text>
+      </View>
+    )
+  }
+}
+
+export default WelcomePage
