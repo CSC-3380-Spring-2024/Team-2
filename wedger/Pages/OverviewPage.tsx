ce0e62153239be48f0de6634124910a8590b6f45
ohderin <contact@derin.io>
2024-04-12 14:33:48 -0500

    Update OverviewPage.tsx

--- wedger/Pages/OverviewPage.tsx
+++ wedger/Pages/OverviewPage.tsx
@@ -1,43 +1,61 @@
-import {StyleSheet, StatusBar, Text, View, ScrollView, SafeAreaView} from 'react-native';
+import {
+  StyleSheet,
+  StatusBar,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import React, {useState} from 'react';
 import StyledButton from '../Components/StyledButton';
 import PopupModal from '../Components/PopupModal';
-import { LinearGradient } from 'react-native-linear-gradient';
+import {LinearGradient} from 'react-native-linear-gradient';
 import PieChart from 'react-native-pie-chart';
+import {useNavigation} from '@react-navigation/native';
 
 //import { color } from '@rneui/base';
 
 // #2F88bd color for original blue
 
 function OverviewPage() {
-  const navigator = useNavigation()
+  const navigator = useNavigation();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   return (
-    <SafeAreaView style = {styles.container}>
-      <ScrollView style = {styles.ScrollView}>
-      <LinearGradient colors = {['#EBF8FE', '#8eb2c0',]} style = {styles.linearGradient}> 
-        <View style = {styles.container}>
-          < Text style = {styles.header1}> Overview <Text style = {styles.header2}>{GetDate()}</Text></Text> 
-          <View style = {styles.budgetBox}>
-            <View >
-              <Text style = {styles.header2}>Budget Name</Text>
-              <PieChart widthAndHeight={widthAndHeight} series={series} sliceColor={sliceColor} style = {styles.pieChart} />
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.ScrollView}>
+        <LinearGradient
+          colors={['#EBF8FE', '#8eb2c0']}
+          style={styles.linearGradient}>
+          <View style={styles.container}>
+            <Text style={styles.header1}>
+              {' '}
+              Overview <Text style={styles.header2}>{GetDate()}</Text>
+            </Text>
+            <View style={styles.budgetBox}>
+              <View>
+                <Text style={styles.header2}>Budget Name</Text>
+                <PieChart
+                  widthAndHeight={widthAndHeight}
+                  series={series}
+                  sliceColor={sliceColor}
+                  style={styles.pieChart}
+                />
+              </View>
             </View>
-          </View>
-          <View style = {styles.amountBox}>
-            <Text style = {styles.header2}>Amount Left</Text>
-          </View>
-          <View style = {styles.addExpense}>
-            <Text style = {styles.header2}>Add Expense</Text>
-          </View>
-          <View style = {styles.pastExpenses}>
-            <Text style= {styles.header2}>Past Expenses</Text>
-          </View>
+            <View style={styles.amountBox}>
+              <Text style={styles.header2}>Amount Left</Text>
+            </View>
+            <View style={styles.addExpense}>
+              <Text style={styles.header2}>Add Expense</Text>
+            </View>
+            <View style={styles.pastExpenses}>
+              <Text style={styles.header2}>Past Expenses</Text>
+            </View>
             <StyledButton
               onPress={() => {
                 setModalOpen(true);
-            }}>
+              }}>
               Test test
             </StyledButton>
             <PopupModal
@@ -45,10 +63,10 @@ function OverviewPage() {
               description="Test popup"
               firstButtonPress={() => {
                 setModalOpen(!modalOpen);
-            }}
+              }}
               firstButtonText="Continue"
-          />
-        </View>
+            />
+          </View>
         </LinearGradient>
       </ScrollView>
     </SafeAreaView>
@@ -56,17 +74,29 @@ function OverviewPage() {
 }
 
 function GetDate() {
-  const monthNames: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let monthIndex = (new Date().getMonth());
-  let thisYear = (new Date().getFullYear());
+  const monthNames: string[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  let monthIndex = new Date().getMonth();
+  let thisYear = new Date().getFullYear();
   let monthName = monthNames[monthIndex];
-  return monthName + " " + thisYear;
-
+  return monthName + ' ' + thisYear;
 }
-const widthAndHeight = 225
-const series = [123, 534, 231]
-const sliceColor = ['#7FB5C1', '#C4D2DF', '#2C8FA2']
-const styles = StyleSheet.create ({
+const widthAndHeight = 225;
+const series = [123, 534, 231];
+const sliceColor = ['#7FB5C1', '#C4D2DF', '#2C8FA2'];
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginVertical: 0,
@@ -107,7 +137,7 @@ const styles = StyleSheet.create ({
     marginBottom: 8,
     alignSelf: 'center',
     borderWidth: 0,
-    borderColor: '#2F88bd',// #1E303C black border hex code
+    borderColor: '#2F88bd', // #1E303C black border hex code
     borderRadius: 30,
     backgroundColor: '#FFFFFF',
     width: '85%',
@@ -150,6 +180,6 @@ const styles = StyleSheet.create ({
     width: '85%',
     height: 300,
   },
-})
+});
 
 export default OverviewPage;
