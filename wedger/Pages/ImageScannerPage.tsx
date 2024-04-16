54c37321a7cdaa0f97d2294d7e8d41fc56873d85
ohderin <contact@derin.io>
2024-04-15 20:06:33 -0500

    moved to function over class

--- wedger/Pages/ImageScannerPage.tsx
+++ wedger/Pages/ImageScannerPage.tsx
@@ -1,4 +1,5 @@
-import React, {PureComponent} from 'react';
+'use strict';
+import React, {useRef} from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -8,47 +9,49 @@ import {
 } from 'react-native';
 import {RNCamera} from 'react-native-camera';
 
-class ImageScannerPage extends PureComponent {
-  private camera: any;
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          captureAudio={false}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({barcodes}) => {
-            console.log(barcodes);
-          }}
-        />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-          <TouchableOpacity
-            onPress={this.takePicture.bind(this)}
-            style={styles.capture}>
-            <Text style={{fontSize: 14}}> SNAP </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
+function ImageScannerPage() {
+  const cameraRef = useRef(null);
 
-  takePicture = async () => {
-    if (this.camera) {
-      const options = {quality: 0.5, base64: true};
-      const data = await this.camera.takePictureAsync(options);
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = {
+        quality: 0.2,
+        base64: false,
+        exif: false,
+        skipProcessing: true,
+      };
+      const [data] = await Promise.all([
+        cameraRef.current.takePictureAsync(options),
+      ]);
       console.log(data.uri);
     }
   };
+
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        ref={cameraRef}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        captureAudio={false}
+        onGoogleVisionBarcodesDetected={({barcodes}) => {
+          console.log(barcodes);
+        }}
+      />
+      <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+        <TouchableOpacity onPress={takePicture} style={styles.capture}>
+          <Text style={{fontSize: 14}}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -73,5 +76,5 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('App', () => ImageScannerPage);
+AppRegistry.registerComponent('ImageScannerPage', () => ImageScannerPage);
 export default ImageScannerPage;
