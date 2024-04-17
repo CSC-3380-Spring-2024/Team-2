cb0ecd7ca4d9f0b98892f980f764decf27d7eb60
Max Shroyer <91408516+Shark9y@users.noreply.github.com>
2024-04-17 12:40:21 -0500

    UGHGHGHGHHG

--- wedger/Pages/ShoppingListPage.tsx
+++ wedger/Pages/ShoppingListPage.tsx
@@ -1,14 +1,29 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import {View, Text} from 'react-native';
+import React, {useMemo} from 'react';
+import {useBudget} from '../Context/userBudgetContext';
+import CarouselView from '../Components/CarouselView';
+import {useAuth} from '../Context/userAuthContext';
 
-export class ShoppingListPage extends Component {
-  render() {
-    return (
-      <View>
-        <Text>ShoppingListPage</Text>
-      </View>
-    )
-  }
-}
+const ShoppingListPage = () => {
+  const {getUsersBudgets} = useBudget();
+  const {userData} = useAuth();
+  const budgets = useMemo(() => getUsersBudgets(), [userData]);
+  return (
+    <>
+      {budgets.length > 1 ? (
+        <CarouselView contentArray={budgets}>
+          <View>
+            <Text>ShoppingListPage</Text>
+          </View>
+        </CarouselView>
+      ) : (
+        <View>
+          <Text>ShoppingListPage, budget 1 or null</Text>
+        </View>
+      )}
+    </>
+  );
+};
 
-export default ShoppingListPage
+export default ShoppingListPage;
