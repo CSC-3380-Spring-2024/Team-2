bed1ada46768c2943f3d1ca3dfe178ff59df3ebd
Max Shroyer <91408516+Shark9y@users.noreply.github.com>
2024-04-17 17:37:21 -0500

    AROOGA

--- wedger/Pages/ShoppingListPage.tsx
+++ wedger/Pages/ShoppingListPage.tsx
@@ -1,28 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import {View, Text} from 'react-native';
-import React, {useMemo} from 'react';
-import {useBudget} from '../Context/userBudgetContext';
-import CarouselView from '../Components/CarouselView';
-import {useAuth} from '../Context/userAuthContext';
+import React from 'react';
 
 const ShoppingListPage = () => {
-  const {getUsersBudgets} = useBudget();
-  const {userData} = useAuth();
-  const budgets = useMemo(() => getUsersBudgets(), [userData]);
   return (
-    <>
-      {budgets.length > 1 ? (
-        <CarouselView contentArray={budgets}>
-          <View>
-            <Text>ShoppingListPage</Text>
-          </View>
-        </CarouselView>
-      ) : (
-        <View>
-          <Text>ShoppingListPage, budget 1 or null</Text>
-        </View>
-      )}
-    </>
+    <View>
+      <Text>ShoppingListPage, budget 1 or null</Text>
+    </View>
   );
 };
 
